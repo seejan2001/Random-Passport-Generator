3be9009ee39326dc24597e9ca4aec1d5cabe395c
seejan2001 <111683283+seejan2001@users.noreply.github.com>
2022-09-12 14:27:07 +0545

    Updated

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-
-
 let userNumber = document.getElementById('userNumber');
 let submit = document.getElementById('btn');
+let inputProcessed = document.getElementById('inputProcessedData');
 let data;              // Translate string to integer
 let i=0;              //Initial loop
 let random=0;        // Generate random value;
 let randomData=0;   // Storage of data;
 var output;
+var length;         // Contain size of the string;
+let text;           // Currently Not used
+let a;          // Currently Not used
 
 function getDataOnClick(){
     data = parseInt(userNumber.value);
@@ -23,8 +25,10 @@ function getDataOnClick(){
     }
 
 }
-output = randomData;
-console.log(typeof(output));
+output = randomData +''+ randomData; // Incase if i need to translate into alphabet(ASCII);
+console.log(output);                    
+inputProcessed.value = randomData;  
+length = output.length;          // Give length of the string
 a = parseInt(output);           // converting string to number
 convertToCharacter(a);         // function calling to convert number into character
 
@@ -34,11 +38,14 @@ random =0;
 
 }  
     //Second funciton to convert integer into random value;
-function convertToCharacter(a){
+function convertToCharacter(a){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+    // console.log(typeof(a));
+     
+    //  console.log(text);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
 
 }
 
 submit.addEventListener('click',()=>{
-    getDataOnClick();
+    getDataOnClick();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 })
 
